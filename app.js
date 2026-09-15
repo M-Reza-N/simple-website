5e5937521910bc0b3f3b0bbb72c37f62e7d2ac5d
M.Reza <rezanadermanesh5@gmail.com>
2026-09-15 13:39:52 +0330

    i selected some tag in js

---
+++ app.js
@@ -0,0 +1,4 @@
+let pElem = document.querySelector("p");
+let divElem = document.querySelector("div");
+let headerElem = document.querySelector("header");
+let spanElem = document.querySelector("span");
